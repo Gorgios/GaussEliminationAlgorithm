a208eb3a94c65cdb85436d8d7932d38ddb944737
Gorgios <jurek.r98@gmail.com>
2019-11-11 21:22:54 +0100

    Complete without and particial choice

--- src/com/ug/Main.java
+++ src/com/ug/Main.java
@@ -3,7 +3,7 @@ package com.ug;
 public class Main {
 
     public static void main(String[] args) {
-       int size = 2000;
+       int size = 10;
        RandomGenerator intMatrix[][];
        RandomGenerator intVector[][];
        intMatrix=RandomGenerator.generateMatrix(size);
@@ -15,25 +15,40 @@ public class Main {
        Matrix vectorCopyF = new Matrix(1,size,Float.class,intVector);
        Matrix vectorCheckF;
         //double
+
         Matrix matrixD = new Matrix(size,size,Double.class,intMatrix);
         Matrix matrixCopyD = new Matrix(size,size,Double.class,intMatrix);
         Matrix vectorD = new Matrix(1,size,Double.class,intVector);
         Matrix vectorCopyD = new Matrix(1,size,Double.class,intVector);
         Matrix vectorCheckD;
+
         matrixF.generateMatrix();
         matrixCopyF.generateMatrix();
         vectorF.generateMatrix();
         vectorCopyF.generateMatrix();
+
        matrixD.generateMatrix();
        matrixCopyD.generateMatrix();
        vectorD.generateMatrix();
        vectorCopyD.generateMatrix();
+
        vectorCheckF= matrixF.methodWithoutChoice(matrixF,vectorF,matrixCopyF);
-       vectorCheckD= matrixD.methodWithoutChoice(matrixD,vectorD,matrixCopyD);
-
-
         System.out.println(ErrorCounter.floatError(vectorCopyF,vectorCheckF,size));
-        System.out.println(ErrorCounter.doubleError(vectorCopyD,vectorCheckD,size));
+        matrixF = new Matrix(size,size,Float.class,intMatrix);
+        matrixCopyF = new Matrix(size,size,Float.class,intMatrix);
+        vectorF = new Matrix(1,size,Float.class,intVector);
+        vectorCopyF = new Matrix(1,size,Float.class,intVector);
+        matrixF.generateMatrix();
+        matrixCopyF.generateMatrix();
+        vectorF.generateMatrix();
+        vectorCopyF.generateMatrix();
+        vectorCheckF= matrixF.methodWithPartialChoice(matrixF,vectorF,matrixCopyF);
+        vectorCheckD= matrixD.methodWithPartialChoice(matrixD,vectorD,matrixCopyD);
+
+      //  vectorCopyF.printMatrix();
+     //   vectorCheckF.printMatrix();
+       System.out.println(ErrorCounter.floatError(vectorCopyF,vectorCheckF,size));
+       System.out.println(ErrorCounter.doubleError(vectorCopyD,vectorCheckD,size));
        //System.out.println(Tests.isMatrixOk(4000));
     }
 }
--- src/com/ug/Matrix.java
+++ src/com/ug/Matrix.java
@@ -1,11 +1,11 @@
 package com.ug;
 
 public class Matrix<T extends Number> {
-     T[][] matrix;
-     int columns;
-     int rows;
-     Class<T> type;
-     RandomGenerator[][] intMatrix;
+    T[][] matrix;
+    int columns;
+    int rows;
+    Class<T> type;
+    RandomGenerator[][] intMatrix;
 
     public Matrix(int columns, int rows, Class<T> type, RandomGenerator[][] intMatrix) {
         this.columns = columns;
@@ -39,52 +39,19 @@ public class Matrix<T extends Number> {
             }
     }
 
-    public void printMatrix() {
-        System.out.println("MACIERZ: ");
-        for (int j = 0; j < rows; j++) {
-            for (int i = 0; i < columns; i++)
-                System.out.print(matrix[j][i] + "  ");
-            System.out.print("\n");
-        }
-    }
-
-    public Matrix<T> methodWithoutChoice(Matrix<T> matrix, Matrix<T> vector, Matrix<T> MatrixCopy) {
+    public void elimination(Matrix<T> matrix, Matrix<T> vector, int i) {
         int n = vector.rows;
-        Matrix<T> resultVector = new Matrix(n, type);
-        Matrix<T> checkingVector = new Matrix(n, type);
         if (type.equals(Float.class)) {
-            float multiple ;
-            for (int i = 0; i < n; i++) {
+            float multiple;
                 for (int j = i + 1; j < n; j++) {
                     multiple = matrix.matrix[j][i].floatValue() / matrix.matrix[i][i].floatValue();
                     vector.matrix[j][0] = (T) (Float) (vector.matrix[j][0].floatValue() - multiple * vector.matrix[i][0].floatValue());
                     for (int r = i; r < n; r++)
                         matrix.matrix[j][r] = (T) (Float) (matrix.matrix[j][r].floatValue() - multiple * matrix.matrix[i][r].floatValue());
 
-                }
             }
-            for (int i = n - 1; i >= 0; i--) {
-                float sum = 0;
-                for (int j = i + 1; j < n; j++) {
-                    sum += matrix.matrix[i][j].floatValue() * resultVector.matrix[j][0].floatValue();
-                }
-                resultVector.matrix[i][0] = (T) (Float) ((vector.matrix[i][0].floatValue() - sum) / matrix.matrix[i][i].floatValue());
-            }
-
-            for (int i = 0; i < n; i++) {
-                float sum = 0;
-                for (int j = 0; j < n; j++) {
-                    sum += MatrixCopy.matrix[i][j].floatValue() * resultVector.matrix[j][0].floatValue();
-
-                }
-                checkingVector.matrix[i][0] = (T) (Float) (sum);
-            }
-
-       //    resultVector.printMatrix();
-        }
-        else if (type.equals(Double.class)){
-            double multiple ;
-            for (int i = 0; i < n; i++) {
+        } else if (type.equals(Double.class)) {
+            double multiple;
                 for (int j = i + 1; j < n; j++) {
                     multiple = matrix.matrix[j][i].doubleValue() / matrix.matrix[i][i].doubleValue();
                     vector.matrix[j][0] = (T) (Double) (vector.matrix[j][0].doubleValue() - multiple * vector.matrix[i][0].doubleValue());
@@ -92,26 +59,120 @@ public class Matrix<T extends Number> {
                         matrix.matrix[j][r] = (T) (Double) (matrix.matrix[j][r].doubleValue() - multiple * matrix.matrix[i][r].doubleValue());
 
                 }
-            }
-            for (int i = n - 1; i >= 0; i--) {
-                double sum = 0;
-                for (int j = i + 1; j < n; j++) {
-                    sum += matrix.matrix[i][j].doubleValue() * resultVector.matrix[j][0].doubleValue();
-                }
-                resultVector.matrix[i][0] = (T) (Double) ((vector.matrix[i][0].doubleValue() - sum) / matrix.matrix[i][i].doubleValue());
-            }
+        }
+    }
 
+    public Matrix<T> resultVector(Matrix<T> matrix, Matrix<T> resultVector,Matrix<T> vector){
+        int n = vector.rows;
+        if (type.equals(Float.class)) {
+           for (int i = n - 1; i >= 0; i--) {
+               float sum = 0;
+               for (int j = i + 1; j < n; j++) {
+                   sum += matrix.matrix[i][j].floatValue() * resultVector.matrix[j][0].floatValue();
+               }
+               resultVector.matrix[i][0] = (T) (Float) ((vector.matrix[i][0].floatValue() - sum) / matrix.matrix[i][i].floatValue());
+           }
+       }
+        else if (type.equals(Double.class)){
+           for (int i = n - 1; i >= 0; i--) {
+               double sum = 0;
+               for (int j = i + 1; j < n; j++) {
+                   sum += matrix.matrix[i][j].doubleValue() * resultVector.matrix[j][0].doubleValue();
+               }
+               resultVector.matrix[i][0] = (T) (Double) ((vector.matrix[i][0].doubleValue() - sum) / matrix.matrix[i][i].doubleValue());
+           }
+
+       }
+        return resultVector;
+    }
+    public Matrix<T> checkVector(Matrix<T> matrixCopy, Matrix<T> resultVector,Matrix<T> checkingVector,Matrix<T> vector){
+        int n = vector.rows;
+        if (type.equals(Float.class)) {
+            for (int i = 0; i < n; i++) {
+                float sum = 0;
+                for (int j = 0; j < n; j++) {
+                    sum += matrixCopy.matrix[i][j].floatValue() * resultVector.matrix[j][0].floatValue();
+
+                }
+                checkingVector.matrix[i][0] = (T) (Float) (sum);
+            }
+        }
+        else if (type.equals(Double.class)){
             for (int i = 0; i < n; i++) {
                 double sum = 0;
                 for (int j = 0; j < n; j++) {
-                    sum += MatrixCopy.matrix[i][j].doubleValue() * resultVector.matrix[j][0].doubleValue();
+                    sum += matrixCopy.matrix[i][j].doubleValue() * resultVector.matrix[j][0].doubleValue();
 
                 }
                 checkingVector.matrix[i][0] = (T) (Double) (sum);
             }
-
-
         }
         return checkingVector;
+    }
+    public void switchMatrix(Matrix<T> matrix,Matrix<T> vector,int max, int i){
+        for (int j = 0; j < matrix.columns; j++) {
+            T temp = matrix.matrix[i][j];
+            matrix.matrix[i][j] = matrix.matrix[max][j];
+            matrix.matrix[max][j] = temp;
+        }
+        T temp = vector.matrix[i][0];
+        vector.matrix[i][0] = vector.matrix[max][0];
+        vector.matrix[max][0] = temp;
+    }
+    public Matrix<T> methodWithoutChoice(Matrix<T> matrix, Matrix<T> vector, Matrix<T> MatrixCopy) {
+        int n = vector.rows;
+        Matrix<T> resultVector = new Matrix(n, type);
+        Matrix<T> checkingVector = new Matrix(n, type);
+        for (int i=0; i<n; i++)
+            elimination(matrix,vector,i);
+        resultVector=resultVector(matrix,resultVector,vector);
+       // resultVector.printMatrix();
+        checkingVector = checkVector(MatrixCopy,resultVector,checkingVector,vector);
+
+        return checkingVector;
+    }
+
+    public Matrix<T> methodWithPartialChoice(Matrix<T> matrix, Matrix<T> vector, Matrix<T> MatrixCopy) {
+        int n = vector.rows;
+        Matrix<T> resultVector = new Matrix(n, type);
+        Matrix<T> checkingVector = new Matrix(n, type);
+        if (type.equals(Float.class)) {
+            int max;
+            for (int i = 0; i < n; i++) {
+                max = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (Math.abs(matrix.matrix[j][i].floatValue()) > Math.abs(matrix.matrix[max][i].floatValue())) {
+                        max = j;
+                    }
+                }
+                switchMatrix(matrix, vector, max, i);
+                elimination(matrix, vector,i);
+            }
+        }
+        if (type.equals(Double.class)) {
+            int max;
+            for (int i = 0; i < n; i++) {
+                max = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (Math.abs(matrix.matrix[j][i].doubleValue()) > Math.abs(matrix.matrix[max][i].doubleValue())) {
+                        max = j;
+                    }
+                }
+                switchMatrix(matrix, vector, max, i);
+                elimination(matrix, vector,i);
+            }
+        }
+        resultVector = resultVector(matrix, resultVector, vector);
+       // resultVector.printMatrix();
+        checkingVector = checkVector(MatrixCopy, resultVector, checkingVector, vector);
+        return checkingVector; }
+
+    public void printMatrix() {
+        System.out.println("MACIERZ: ");
+        for (int j = 0; j < rows; j++) {
+            for (int i = 0; i < columns; i++)
+                System.out.print(matrix[j][i] + "  ");
+            System.out.print("\n");
+        }
     }
 }
